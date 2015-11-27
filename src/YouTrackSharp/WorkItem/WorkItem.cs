81e9b88a4019aac690c9e651dcb496037ccfa376
martinf <fabik.martin@gmail.com>
2015-11-27 23:45:01 +0100

    Add WorkItemManagement, gets the list of available work items for a particular issue.(Time Tracking)

---
+++ src/YouTrackSharp/WorkItem/WorkItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using JsonFx.Json;
+
+namespace YouTrackSharp.WorkItem
+{
+    public class WorkItem
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+
+        [JsonName("author")]
+        public object AuthorObject { get; set; }
+
+        [JsonName("date")]
+        public long EpochDate { get; set; }
+
+        public DateTime Date
+        {
+            get
+            {
+                var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                return epoch.AddMilliseconds(EpochDate);
+            }
+        }
+
+        public string AuthorName
+        {
+            get
+            {
+                return (string)((IDictionary<string, object>)AuthorObject)["login"];
+            }
+        }
+    }
+}
---
+++ src/YouTrackSharp/WorkItem/WorkItemManagement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using YouTrackSharp.Infrastructure;
+
+namespace YouTrackSharp.WorkItem
+{
+    public class WorkItemManagement
+    {
+        private readonly IConnection _connection;
+
+        public WorkItemManagement(IConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<WorkItem> GetWorkItems(string issueId)
+        {
+            return _connection.Get<IEnumerable<WorkItem>>(string.Format("issue/{0}/timetracking/workitem", issueId));
+        } 
+    }
+}
